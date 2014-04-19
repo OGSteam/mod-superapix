ecdf051e0d10f3443862236cabce86597bad32b5
machine <gounter_lievin@yahoo.fr>
2014-04-19 06:57:57 +0000

    remplacement xcopy par stream_copy
    cross_domain.php edited online with Bitbucket

--- vue/cross_domain.php
+++ vue/cross_domain.php
@@ -94,7 +94,19 @@ if ($must_down)
     
     //file_put_contents(MOD_ROOT_XML.$pub_url.'.xml', file_get_contents($url));
         
-    copy($url, MOD_ROOT_XML.$pub_url.'.xml');
+ function stream_copy($src, $dest)
+    {
+        $fsrc = fopen($src,'r');
+        $fdest = fopen($dest,'w+');
+        $len = stream_copy_to_stream($fsrc,$fdest);
+        fclose($fsrc);
+        fclose($fdest);
+        return $len;
+    } 
+
+	
+    stream_copy($url, MOD_ROOT_XML.$pub_url.'.xml'); // xcopy buggant ...
+    //copy($url, MOD_ROOT_XML.$pub_url.'.xml');
 }
  //  $url = uni_replace(find_config('uni'),$url);
   
