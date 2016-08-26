75eb9e1013b20fccbff227105259189ff7c82fd3
machine <gounter_lievin@yahoo.fr>
2016-08-26 16:48:27 +0200

    Desactivation ancienne interface visuelle Travail prepa xtense

---
+++ model/xtense.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @package [Mod] Superapix
+ * @author Machine
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+
+if (!defined('IN_SPYOGAME') || !defined('IN_SUPERAPIX'))
+    die("Hacking attempt");
+
+
+function IsXtenseInstalled()
+{
+    
+}
+
+function AddCallback($appels)
+{
+    
+}
+
+function DelCallback($appels)
+{
+    
+}
+
+
+function DelAllCallbacks($appels)
+{
+    
+}
+
+
+function GetAllCallBacks()
+{
+    
+}
+
