6c64ea416da50e9420c47d44293100cf2d2fa07c
machine <gounter_lievin@yahoo.fr>
2016-08-27 20:19:15 +0200

    quelques fix ajout de l'installation des callbacks via l''admin

--- model/config.php
+++ model/config.php
@@ -48,11 +48,13 @@ function create_uni_vide() {
 
 function defineDebug() {
     global $db;
-    // Check if xtense_callbacks table exists :
+
+            // Check if xtense_callbacks table exists :
     $query = 'SHOW TABLES LIKE "' . TABLE_CFG . '"';
     $result = $db->sql_query($query);
     if ($db->sql_numrows($result) != 0) {
         define("DEBUG", find_config("debug"));
     }
-    define("DEBUG", 0);
+
+
 }
